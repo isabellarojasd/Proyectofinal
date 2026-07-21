6c9352062b72b173914260cf508cf3aa8a32e666
isa421 <isabellarojasduque.com@gmail.com>
2026-07-21 11:15:10 -0500

    Interfaces apenas empezadas

---
+++ src/main/java/org/example/ManejoInventario.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import javax.swing.*;
+
+public class ManejoInventario {
+    private JTextField txtBusqueda;
+    private JButton btnBuscar;
+    private JTextField txtNombre;
+    private JTextField txtDescrpcion;
+    private JTextField txtPrecio;
+    private JTextField txtStock;
+    private JTable tblProductos1;
+}
