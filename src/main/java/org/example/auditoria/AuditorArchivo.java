da677a4f5fb98e9a1fc6be33f48857ee97899b93
isabelladuque-cmyk <isabella.duque@autonoma.edu.co>
2026-07-22 16:08:59 -0500

    Clase producto listo

--- src/main/java/org/example/auditoria/AuditorArchivo.java
+++ src/main/java/org/example/auditoria/AuditorArchivo.java
@@ -1,4 +1,11 @@
 package org.example.auditoria;
+import lombok.Data;
+
+@Data
 
 public class AuditorArchivo {
+    private String nombre;
+
+    AuditorArchivo a = new AuditorArchivo();
 }
+
--- src/main/java/org/example/modelo/Producto.java
+++ src/main/java/org/example/modelo/Producto.java
@@ -1,4 +1,15 @@
 package org.example.modelo;
+import lombok.*;
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 
 public class Producto {
+    private String nombre;
+    private String descripcion;
+    private String precio;
+    private int cantidadDisponible;
+    private int id;
+
 }
+
