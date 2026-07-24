9cfe7dcbdea0a59bc49a55a0e6a7593708250a13
isabelladuque-cmyk <isabella.duque@autonoma.edu.co>
2026-07-24 13:08:00 -0500

    ControladorClientes, clase finalizada

--- src/main/java/org/example/servidor/ControladorClientes.java
+++ src/main/java/org/example/servidor/ControladorClientes.java
@@ -1,14 +1,35 @@
 package org.example.servidor;
+import org.example.protocolo.Peticion;
+import org.example.protocolo.Respuesta;
 
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.net.Socket;
 
 public class ControladorClientes implements Runnable{
 
     private Socket socket;
-    private DespachadorPeticion peticion;
+    private DespachadorPeticion despachador;
+
+    public ControladorClientes(Socket socket, DespachadorPeticion despachador) {
+        this.socket = socket;
+        this.despachador = despachador;
+    }
 
     @Override
     public void run() {
+        try {
+            ObjectInputStream objectEntrada = new ObjectInputStream(socket.getInputStream());
+             ObjectOutputStream objectSalida = new ObjectOutputStream(socket.getOutputStream()) ;
+            Peticion peticion = (Peticion) objectEntrada.readObject();
+            Respuesta respuesta = despachador.procesar(peticion);
+            objectSalida.writeObject(respuesta);
+        } catch (IOException | ClassNotFoundException e) {
+            System.out.println("Ocurrio un error al intentar conectar" + e.getMessage());
+        }
+
 
     }
 }
+
