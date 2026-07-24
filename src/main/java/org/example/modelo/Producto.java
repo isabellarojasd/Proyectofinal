6f47e05585166b2d1a3de85efa4028ac5366e486
isabelladuque-cmyk <isabella.duque@autonoma.edu.co>
2026-07-24 16:59:41 -0500

    JavaDoc listo clase Producto

--- src/main/java/org/example/modelo/Producto.java
+++ src/main/java/org/example/modelo/Producto.java
@@ -1,15 +1,44 @@
 package org.example.modelo;
 import lombok.*;
+
+/**
+ * Representa un producto dentro del inventario
+ * Tiene los datos del producto: nombre, descripcion, precio, la cantidad disponible y su codigo (id)
+ * No tiene lógica de negocio;
+ * Las validaciones y operaciones sobre estos productos se encuentran en la clase Inventario.
+ * @author Isabella Duque E
+ */
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @Setter
 public class Producto {
+
+    /**
+     * Nombre del producto
+     */
     private String nombre;
+
+    /**
+     * Descripcion detallada del producto
+     */
     private String descripcion;
+
+    /**
+     * Precio por unidad del producto
+     */
     private Double precio;
+
+    /**
+     * Cantidad disponible del producto en el inventario
+     */
     private int cantidadDisponible;
+
+    /**
+     * Identificador único del producto, asignado automaticamente
+     * por la clase Inventario al momento de agregarlo
+     */
     private int id;
 
 
